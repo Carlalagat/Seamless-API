da12b70bd0bbcd9f6df1d1a2706a1f2394622256
Carla Lagat <carlacheruto3@gmail.com>
2025-02-24 17:30:39 +0300

    create folder  structure

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-import Express from 'express';
+import Express, { Router } from 'express';
+// const router = Express.Router();
+// router.get('/', (req, res) => {
+//     res.send('Hello World');
+//     });
 const app = Express();
-app.get('/', (req, res) => {
-    res.send('Hello World');
-    });
+
+
+
 app.listen(3000, () => {
     console.log('Server is running: on http://localhost:3000');
     });
