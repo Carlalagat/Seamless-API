ede8a92f7d840dc1b9984443f023c72bf6c3af60
owenga <owengajoseph54@gmail.com>
2025-02-26 11:20:06 +0300

    created file format and some starting building blocks

---
+++ src/helpers/status-codes.js
@@ -0,0 +1,15 @@
+// /src/helpers/statusCodes.js
+/*
+this code serves as a starting block for creating enums like status codes) and planning additional helper functions with examples. 
+ */
+const STATUS_CODES = {
+    OK: 200,
+    CREATED: 201,
+    BAD_REQUEST: 400,
+    UNAUTHORIZED: 401,
+    FORBIDDEN: 403,
+    NOT_FOUND: 404,
+    INTERNAL_SERVER_ERROR: 500,
+};
+
+module.exports = STATUS_CODES;
