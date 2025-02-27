602cfc34a0612b0156a851ceda2ac3ac0949cd13
G-KIBICHII <kibichiigilbert54@gmail.com>
2025-02-26 22:24:01 -0800

    handling error function

--- src/models/users.models.js
+++ src/models/users.models.js
@@ -1,18 +1,20 @@
 // ?add the structure of the data of user comming from the form 
 // list all the orr drfine how all properties will look like 
 const mongoose  = require('mongoose')
+const {isEmail} = require("validator")
 const userSchema = new mongoose.Schema({
     email:{
         type:String,
-        required:true,
+        required:[true,"email is required"],
         unique:true,
-        lowercase:true
+        lowercase:true,
+        validate:[isEmail,"please enter a valid email"]
     },
     password:{
         type:String,
-        required:true,
+        required:[true,"password is a required field"],
         unique:true,
-        minLength:6
+        minlength:[6,"minimum password length is 6 character"]
     }
 })
 
