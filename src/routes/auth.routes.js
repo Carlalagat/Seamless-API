c1d03645b41c8d48b0d8632f9784171a2bef74e8
Ian Ndegwa <ndegwaian241@gmail.com>
2025-03-03 21:50:38 +0300

    message

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -6,7 +6,7 @@ const { verifyToken } = require("../middleware/auth.middleware");
 router.post('/signup', authController.signup);
 router.post('/signin', authController.signin);
 
-// Example: Protect a user profile route
+// Example: Protect a Client profile route
 router.get("/profile", verifyToken, async (req, res) => {
     res.json({ message: "Welcome to your profile!", user: req.user });
   });
