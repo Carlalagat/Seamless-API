d0af28cd27cf07ef474c7b604f6f552800938a39
Ian Ndegwa <ndegwaian241@gmail.com>
2025-03-04 08:01:05 +0300

    message

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -3,13 +3,18 @@ require("dotenv").config();
 
 exports.verifyToken = (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1];
-  if (!token) return res.status(403).json({ message: "Access denied, token missing" });
+  if (!token) {
+    return res.status(403).json({ message: "Access denied, token missing" });
+  }
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
     next();
   } catch (error) {
+    if (error.name === "TokenExpiredError") {
+      return res.status(401).json({ message: "Token expired" });
+    }
     res.status(401).json({ message: "Invalid token" });
   }
 };
