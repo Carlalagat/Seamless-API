5409b0692ef4ac865ac330105225ee49f865bcf4
Eric <njukieric233@gmail.com>
2025-03-13 12:52:01 +0300

    Eric added the fabrictype controll, route, services and last but not least index route

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,6 +7,7 @@ const productRoutes = require('./products.routes');
 const orderRoutes = require('./order.routes');
 const fabricRoutes = require('./fabric.routes');
 const reviewRoutes = require('./review.routes');
+const fabrictypeRoutes = require('./fabrictype.routes');
 
 router.use('/auth', authRoutes);
 router.use('/users', userRoutes);
@@ -14,5 +15,6 @@ router.use('/products', productRoutes);
 router.use('/orders', orderRoutes);
 router.use('/fabric', fabricRoutes);
 router.use('/review', reviewRoutes);
+router.use('/fabrictypes', fabrictypeRoutes);
 
 module.exports = router;
